ef8c97f3ce480dce407bb3b3aa9388d12f0defb0
dchenmei <dchenmei@users.noreply.github.com>
2018-07-04 16:46:49 -0400

    Added inherited move method to every children class to prevent compiler error

--- chinese-chess/pieces.h
+++ chinese-chess/pieces.h
@@ -7,7 +7,7 @@ namespace chinese_chess
 	{
 	  public:
 		Piece (int px, int py, bool r): x(px), y(py), red(r) {}
-		virtual void move(int, int) const = 0;
+		virtual void move(int, int) = 0;
 
 	  private:
 		int x;
@@ -18,6 +18,7 @@ namespace chinese_chess
     class General: public Piece
 	{
 	  public:
+		General(int px, int py, bool color): Piece(px, py, color) {}
 		void move(int, int);
 
 	  private:
@@ -27,36 +28,42 @@ namespace chinese_chess
 	class Advisor: public Piece
 	{
 	  public:
+		Advisor(int px, int py, bool color): Piece(px, py, color) {}
 		void move(int, int);
 	};
 
 	class Elephant: public Piece
 	{
 	  public:
+		Elephant(int px, int py, bool color): Piece(px, py, color) {}
 		void move(int, int);
 	};
 
 	class Horse: public Piece
 	{
 	  public:
+		Horse(int px, int py, bool color): Piece(px, py, color) {}
 		void move(int, int);
 	};
 
 	class Chariot: public Piece
 	{
 	  public:
+		Chariot(int px, int py, bool color): Piece(px, py, color) {}
 		void move(int, int);
 	};
 
 	class Cannon: public Chariot
 	{
 	  public:
+		Cannon(int px, int py, bool color): Chariot(px, py, color) {}
 		void move(int, int);
 	};
 
 	class Soldier: public Piece
 	{
 	  public:
+		Soldier(int px, int py, bool color): Piece(px, py, color) {}
 		void move(int, int);
 	};
 }
