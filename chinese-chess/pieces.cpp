05de2949d76dd30aacfbf12a20c72cd7071a80e5
dchenmei <dchenmei@users.noreply.github.com>
2018-07-15 16:11:38 -0400

    Modified piece side checks to return a boolean upon successful move

--- chinese-chess/pieces.cpp
+++ chinese-chess/pieces.cpp
@@ -5,21 +5,27 @@ using namespace std; // can we do this ???
 namespace chinese_chess
 {
 	// General	
-	void General::move(int dx, int dy)
+	bool General::move(int dx, int dy)
 	{
 		// Move valid only if:
 		// - not outside of box (check by board)
 		// - not in check (check by board)
 		// - not facing the opponenet's general (check by board)
-		if (!dx != !dy) // basically up down left right but with limited range
-		{
-			x += dx;	
-			y += dy;
-		}
+		// - up down left right with distance of one
+		
+		// violate move if:
+		// attempt diagonal move
+		// attempt move distance greater than one
+		if ((dx && dy) || (abs(dx) != 1 && abs(dy) != 1))
+			return false;
+
+		x += dx;
+		y += dy;
+		return true;
 	}
 
 	// Advisor
-	void Advisor::move(int dx, int dy)
+	bool Advisor::move(int dx, int dy)
 	{
 		// Move valid only if:
 		// - not outside of box (checked by board)
@@ -29,16 +35,16 @@ namespace chinese_chess
 		// 	 * x - 1, y - 1 
 		// 	 * x - 1, y + 1 
 	
-		// TODO: Check that this logic actually works
-		if (dx * dy == -1 || dx * dy)
-		{
-			x += dx;
-			y += dy;
-		}
+		if ((dx * dy != 1 && dx * dy != -1) || abs(dx) != 1 || abs(dy) != 1)
+			return false;
+
+		x += dx;
+		y += dy;
+		return true;
 	}
 
 	// Elephant
-	void Elephant::move(int dx, int dy)
+	bool Elephant::move(int dx, int dy)
 	{
 		// Move valid only if
 		// - not over the river (checked by board)
@@ -48,50 +54,68 @@ namespace chinese_chess
 		// * x + 2, y + 2
 		// * x - 2, y - 2
 		// * x - 2, y + 2
-		if ((dx == 2 || dx == -2) && (dy == 2 || dy == -2))
-		{
-			x += dx;
-			y += dy;
-		}
+		if (dx != dy || abs(dx) != 2 || abs(dy) != 2)
+			return false;
+
+		x += dx;
+		y += dy;
+		return true;
 	}
 
 	// Horse
-	void Horse::move(int dx, int dy)
+	bool Horse::move(int dx, int dy)
 	{
 		// Move valid only if
 		// - not block by another piece (cannot jump over other piece) -- checked by board
 		// - one point horizontal / verical then one point diagonally
+		// 	 * can also be viewed as two points (u, d, l, r) * two directions
 		// 8 possible combinations
 
+		// TODO: Double check this logic
+		if (abs(dx - dy) != 1)
+			return false;
+
+		x += dx;
+		y += dy;
+		return true;
 	}
 
 	// Chariot
-	void Chariot::move(int dx, int dy)
+	bool Chariot::move(int dx, int dy)
 	{
 		// Only one direction moves allowed
-		if (!dx != !dy)
-		{
-			x += dx;
-			y += dy;
-		}
+		if (dx && dy)
+			return false;
+
+		x += dx;
+		y += dy;
+		return true;
 	}
 
 	// Cannon
-	void Cannon::move(int dx, int dy)
+	// TODO: refactor to chariot because same implementation
+	bool Cannon::move(int dx, int dy)
 	{
+		if (dx && dy)
+			return false;
+
+		x += dx;
+		y += dy;	
+		return true;
 	}
 
 	// Soldier
-	void Soldier::move(int dx, int dy)
+	bool Soldier::move(int dx, int dy)
 	{
-		// validate that the move is valid
-		if ((!dx && dy) || (across_river && (dx || -dx)))
-		{
-			x += dx;
-			y += dy;
-		}
+		// Not valid if 
+		// - two directions at a time
+		// - moving backwards
+		// - moving horizontally before crossing river
+		if ((!dx && dy) || dx < 0 || (!across_river && dy))
+			return false;
 
-		// invalid move
-		// TODO: throw error
+		x += dx;
+		y += dy;
+		return true;
 	}
 }
--- chinese-chess/pieces.h
+++ chinese-chess/pieces.h
@@ -2,7 +2,9 @@
 #define PIECES_H
 
 #include <string>
+#include <algorithm>
 using std::string;
+using std::abs;
 
 /* IDs for piece types
  * 0 - general
@@ -31,10 +33,11 @@ namespace chinese_chess
 	  public:
 		Piece (int i, int px, int py, string n, bool r): id(i), x(px), y(py), name(n), red(r) {}
 		string get_name() { return name; }
+		int get_id() { return id; }
 		int get_x() { return x; }
 		int get_y() { return y; }
 		bool is_red() { return red; }
-		virtual void move(int, int) = 0;
+		virtual bool move(int, int) = 0;
 
 	 protected:
 		int id;
@@ -48,7 +51,7 @@ namespace chinese_chess
 	{
 	  public:
 		General(int px, int py, bool color): Piece(general, px, py, "General", color) {}
-		void move(int, int);
+		bool move(int, int);
 
 	  private:
 		bool check;	
@@ -58,35 +61,35 @@ namespace chinese_chess
 	{
 	  public:
 		Advisor(int px, int py, bool color): Piece(advisor, px, py, "Advisor", color) {}
-		void move(int, int);
+		bool move(int, int);
 	};
 
 	class Elephant: public Piece
 	{
 	  public:
 		Elephant(int px, int py, bool color): Piece(elephant, px, py, "Elephant", color) {}
-		void move(int, int);
+		bool move(int, int);
 	};
 
 	class Horse: public Piece
 	{
 	  public:
 		Horse(int px, int py, bool color): Piece(horse, px, py, "Horse", color) {}
-		void move(int, int);
+		bool move(int, int);
 	};
 
 	class Chariot: public Piece
 	{
 	  public:
 		Chariot(int px, int py, bool color): Piece(chariot, px, py, "Chariot",  color) {}
-		void move(int, int);
+		bool move(int, int);
 	};
 
 	class Cannon: public Piece
 	{
 	  public:
 		Cannon(int px, int py, bool color): Piece(cannon, px, py, "Cannon",  color) {}
-		void move(int, int);
+		bool move(int, int);
 	};
 
 	class Soldier: public Piece
@@ -96,7 +99,7 @@ namespace chinese_chess
 
 	  public:
 		Soldier(int px, int py, bool color): Piece(soldier, px, py, "Soldier", color), across_river(false) {}
-		void move(int, int);
+		bool move(int, int);
 		void cross_river() { across_river = true; }
 	};
 }
