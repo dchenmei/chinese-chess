c73bfc049b362c7435f6905c65508a1043732b69
dchenmei <dchenmei@users.noreply.github.com>
2018-07-15 12:30:16 -0400

    Added enum ids to easily check piece type

--- chinese-chess/pieces.h
+++ chinese-chess/pieces.h
@@ -4,16 +4,40 @@
 #include <string>
 using std::string;
 
+/* IDs for piece types
+ * 0 - general
+ * 1 - advisor
+ * 2 - elephant
+ * 3 - horse
+ * 4 - chariot
+ * 5 - cannon
+ * 6 - solider
+ */
 namespace chinese_chess
 {
+	enum PieceID
+	{
+		general,
+		advisor,
+		elephant,
+		horse,
+		chariot,
+		cannon,	
+		soldier
+	};
+
 	class Piece
 	{
 	  public:
-		Piece (int px, int py, string n, bool r): x(px), y(py), name(n), red(r) {}
+		Piece (int i, int px, int py, string n, bool r): id(i), x(px), y(py), name(n), red(r) {}
 		string get_name() { return name; }
+		int get_x() { return x; }
+		int get_y() { return y; }
+		bool is_red() { return red; }
 		virtual void move(int, int) = 0;
 
 	 protected:
+		int id;
 		int x;
 		int y;
 		string name;
@@ -23,7 +47,7 @@ namespace chinese_chess
     class General: public Piece
 	{
 	  public:
-		General(int px, int py, bool color): Piece(px, py, "General", color) {}
+		General(int px, int py, bool color): Piece(general, px, py, "General", color) {}
 		void move(int, int);
 
 	  private:
@@ -33,35 +57,35 @@ namespace chinese_chess
 	class Advisor: public Piece
 	{
 	  public:
-		Advisor(int px, int py, bool color): Piece(px, py, "Advisor", color) {}
+		Advisor(int px, int py, bool color): Piece(advisor, px, py, "Advisor", color) {}
 		void move(int, int);
 	};
 
 	class Elephant: public Piece
 	{
 	  public:
-		Elephant(int px, int py, bool color): Piece(px, py, "Elephant", color) {}
+		Elephant(int px, int py, bool color): Piece(elephant, px, py, "Elephant", color) {}
 		void move(int, int);
 	};
 
 	class Horse: public Piece
 	{
 	  public:
-		Horse(int px, int py, bool color): Piece(px, py, "Horse", color) {}
+		Horse(int px, int py, bool color): Piece(horse, px, py, "Horse", color) {}
 		void move(int, int);
 	};
 
 	class Chariot: public Piece
 	{
 	  public:
-		Chariot(int px, int py, bool color): Piece(px, py, "Chariot",  color) {}
+		Chariot(int px, int py, bool color): Piece(chariot, px, py, "Chariot",  color) {}
 		void move(int, int);
 	};
 
 	class Cannon: public Piece
 	{
 	  public:
-		Cannon(int px, int py, bool color): Piece(px, py, "Cannon",  color) {}
+		Cannon(int px, int py, bool color): Piece(cannon, px, py, "Cannon",  color) {}
 		void move(int, int);
 	};
 
@@ -71,7 +95,7 @@ namespace chinese_chess
 		bool across_river; 
 
 	  public:
-		Soldier(int px, int py, bool color): Piece(px, py, "Soldier", color), across_river(false) {}
+		Soldier(int px, int py, bool color): Piece(soldier, px, py, "Soldier", color), across_river(false) {}
 		void move(int, int);
 		void cross_river() { across_river = true; }
 	};
