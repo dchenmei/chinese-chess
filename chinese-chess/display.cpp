cda36c86f402f1e7a84b5903c3ee2c394186c0cf
dchenmei <dchenmei@users.noreply.github.com>
2018-07-18 19:56:02 -0400

    Added lines, albeit not even

--- chinese-chess/display.cpp
+++ chinese-chess/display.cpp
@@ -5,9 +5,13 @@ namespace chinese_chess
 	void Display::init()
 	{
 		sf::RenderWindow window(sf::VideoMode(width, height), "Chinese Chess");
-		sf::RectangleShape rectangle(sf::Vector2f(width - (top_border * 2), height - (left_border * 2)));
+
+		// Board overlay or physically actual board
+		sf::RectangleShape rectangle(sf::Vector2f(width - (left_border * 2), height - (top_border * 2)));
 		rectangle.setPosition(left_border, top_border);
 		rectangle.setFillColor(wood);
+		rectangle.setOutlineThickness(3);
+		rectangle.setOutlineColor(sf::Color::Black);
 
 		while (window.isOpen())
 		{
@@ -20,6 +24,17 @@ namespace chinese_chess
 
 			window.clear(brown);
 			window.draw(rectangle);
+
+			// lines
+			//window.draw(line);
+			for (int i = 1; i <= 8; ++i)
+			{
+				sf::RectangleShape line(sf::Vector2f(width - (left_border * 2), 3));
+				line.setFillColor(sf::Color::Black);
+				line.setPosition(left_border, i * ((height - (left_border * 2)) / 8.0));
+				window.draw(line);
+			}
+
 			window.display();
 		}
 	}
