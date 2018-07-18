7c1653d764a6a22ae6c940d861b188cf4e0addea
dchenmei <dchenmei@users.noreply.github.com>
2018-07-18 19:15:36 -0400

    Created background and board piece overlay

--- chinese-chess/display.cpp
+++ chinese-chess/display.cpp
@@ -4,9 +4,10 @@ namespace chinese_chess
 {
 	void Display::init()
 	{
-		sf::RenderWindow window(sf::VideoMode(width, height), "SFML");
-		sf::CircleShape shape(100.f);
-		shape.setFillColor(sf::Color::Green);
+		sf::RenderWindow window(sf::VideoMode(width, height), "Chinese Chess");
+		sf::RectangleShape rectangle(sf::Vector2f(width - (top_border * 2), height - (left_border * 2)));
+		rectangle.setPosition(left_border, top_border);
+		rectangle.setFillColor(wood);
 
 		while (window.isOpen())
 		{
@@ -17,8 +18,8 @@ namespace chinese_chess
 					window.close();	
 			}
 
-			window.clear();
-			window.draw(shape);
+			window.clear(brown);
+			window.draw(rectangle);
 			window.display();
 		}
 	}
