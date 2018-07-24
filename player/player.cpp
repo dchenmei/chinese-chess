b4d4c699423103edb7d7dd0ecbe016cdb359bed4
dchenmei <dchenmei@users.noreply.github.com>
2018-07-24 19:37:51 -0400

    Started basic layout of player / controller portion

---
+++ player/player.cpp
@@ -0,0 +1,9 @@
+#include "player.h"
+
+namespace chinese_chess
+{
+	void Player::play()
+	{	
+		cout << "P L A Y " << endl;
+	}
+}
--- player/player.h
+++ player/player.h
@@ -1,1 +1,25 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 // controller class that receives input 
+#include "board.h"
+#include <SFML/Graphics.hpp>
+#include <memory>
+
+using std::shared_ptr;
+
+namespace chinese_chess
+{
+	class Player
+	{
+		public:
+			Player(std::shared_ptr<chinese_chess::Board> b) : board(b), red_turn(true) {}
+			//Player(shared_ptr<Board> b, shared_ptr<sf::RenderWindow> w) : window(w), board(b), red_turn(true) {}
+			void play();
+
+		private:
+			shared_ptr<sf::RenderWindow> window;	
+			shared_ptr<Board> board;
+			bool red_turn;
+	};
+}
+#endif
