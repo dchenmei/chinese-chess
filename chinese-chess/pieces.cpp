4c81a64fd54efc97a264afdebc0fd89ffa28f669
dchenmei <dchenmei@users.noreply.github.com>
2018-07-30 13:37:12 -0400

    Added documentation for pieces class

--- chinese-chess/pieces.cpp
+++ chinese-chess/pieces.cpp
@@ -1,19 +1,11 @@
 #include "pieces.h"
 
-using namespace std; // can we do this ???
-
 namespace chinese_chess
 {
 	// General	
 	bool General::move(int dx, int dy)
 	{
-		// Move valid only if:
-		// - not outside of box (check by board)
-		// - not in check (check by board)
-		// - not facing the opponenet's general (check by board)
-		// - up down left right with distance of one
-		
-		// violate move if:
+		// bad move if:
 		// attempt diagonal move
 		// attempt move distance greater than one
 		if ((dx && dy) || (abs(dx) != 1 && abs(dy) != 1))
@@ -27,14 +19,9 @@ namespace chinese_chess
 	// Advisor
 	bool Advisor::move(int dx, int dy)
 	{
-		// Move valid only if:
-		// - not outside of box (checked by board)
-		// - one point diagonally, meaning following combo:
-		// 	 * x + 1, y - 1
-		// 	 * x + 1, y + 1 
-		// 	 * x - 1, y - 1 
-		// 	 * x - 1, y + 1 
-	
+		// bad move if:
+		// outside of box
+		// attempt move distance greather than one
 		if (abs(dx) != abs(dy) || abs(dx) != 1 || abs(dy) != 1)
 			return false;
 
@@ -46,14 +33,9 @@ namespace chinese_chess
 	// Elephant
 	bool Elephant::move(int dx, int dy)
 	{
-		// Move valid only if
-		// - not over the river (checked by board)
-		// - not blocked by another piece (checked by board)
-		// - two points diagonally, meaning following combo:
-		// * x + 2, y - 2
-		// * x + 2, y + 2
-		// * x - 2, y - 2
-		// * x - 2, y + 2
+		// bad move if:
+		// not diagonal
+		// diagonal but not distance of two
 		if (abs(dx) != abs(dy) || abs(dx) != 2 || abs(dy) != 2)
 			return false;
 
@@ -65,17 +47,11 @@ namespace chinese_chess
 	// Horse
 	bool Horse::move(int dx, int dy)
 	{
-		// Move valid only if
-		// - not block by another piece (cannot jump over other piece) -- checked by board
-		// - one point horizontal / verical then one point diagonally
-		// 	 * can also be viewed as two points (u, d, l, r) * two directions
-
 		// Intuition:
 		// - if moving up or down, absolute of left or right is always 1
 		// - if moving left or right, absolute of up or down is always 1
 		// - thus eight possibilities are: (x +- 2, y +- 1) and (x +-1, y +- 2)
 
-		// Only possibilities
 		if ((abs(dx) == 2 && abs(dy) == 1) || (abs(dx) == 1 && abs(dy) == 2))
 		{
 			x += dx;
@@ -83,14 +59,16 @@ namespace chinese_chess
 			return true;
 		}
 
-		// not within the combinations
+		// bad move if:
+		// not within the eight possible combos
 		return false;
 	}
 
 	// Chariot
 	bool Chariot::move(int dx, int dy)
 	{
-		// Only one direction moves allowed
+		// bad move if:
+		// more than one direction at a time
 		if (dx && dy)
 			return false;
 
@@ -99,10 +77,12 @@ namespace chinese_chess
 		return true;
 	}
 
+	// TODO: refactor into same method as Chariot::move
 	// Cannon
-	// TODO: refactor to chariot because same implementation
 	bool Cannon::move(int dx, int dy)
 	{
+		// bad move if:
+		// more than one direction at a time
 		if (dx && dy)
 			return false;
 
@@ -114,7 +94,7 @@ namespace chinese_chess
 	// Soldier
 	bool Soldier::move(int dx, int dy)
 	{
-		// Not valid if:
+		// Bad move if:
 				
 		// moving horizontally before crossing river
 		if (!across_river && dy != 0)
@@ -129,10 +109,9 @@ namespace chinese_chess
 			return false;
 
 		// moves are more than one point
-		if (abs(dx) != 1 && abs(dy) != 1) // TODO: check
+		if (abs(dx) != 1 && abs(dy) != 1) 
 			return false;
 
-		// Else we are good to go:
 		x += dx;
 		y += dy;
 		return true;
